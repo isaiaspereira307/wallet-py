949aaa9e66cc4ced6f70dcd9a629b9f10aef1a1a
isaiaspereira307 <isaias.pereira307@gmail.com>
2022-12-12 14:54:12 -0300

    ajuste para fundamentus

--- main.py
+++ main.py
@@ -1,66 +1,27 @@
-from pandas_datareader import data as web
-import pandas as pd
-import matplotlib.pyplot as plt
-import yfinance as yf
-import datetime
+# get_resultado - https://www.fundamentus.com.br/resultado.php
+# get_papel - https://www.fundamentus.com.br/detalhes.php?papel=WEGE3
+# list_papel_setor - https://www.fundamentus.com.br/resultado.php?setor=27
+
+import fundamentus
+# df = fundamentus.get_resultado()
+# print(df.columns)
+# print(df[df.pl > 0])
 
 
-def ler_empresas():
-    """Ler arquivo xlsx com todas as empresas."""
-    ...
-
-def chamar_cotações_das_empresas(ativo: str):
-    """Chama a cotação da empresa de acordo com os parametros passados."""
-    # data -> MM/DD/AAAA
-    data_inicio = '01/01/2022'
-    data_final = '11/23/2022'
-    source = 'yahoo'
-    sigla = f'^{ativo}'
-    cotacao = web.DataReader('^BVSP', data_source=source, start='10/01/2022', end='10/23/2022')
-    return cotacao
+# df = df[ df.pl  > 0   ]
+# df = df[ df.pl  < 100 ]
+# df = df[ df.pvp > 0   ]
 
 
-def comparar_empresas():
-    """Analisa as 10 melhores empresas."""
-    ...
+df = fundamentus.get_papel('VALE3')  ## or...
+# df = fundamentus.get_papel(['ITSA4','WEGE3'])
+print(df.columns)
+print(df)
 
 
+# fin = fundamentus.list_papel_setor(35)  # finance
+# seg = fundamentus.list_papel_setor(38)  # seguradoras
 
-# cotacao_bovespa = web.DataReader('^BVSP', data_source='yahoo', start='10/01/2022', end='10/23/2022')
-# # cotacao_magalu = web.DataReader('MGLU.SA', data_source='yahoo', start='10/01/2022', end='10/23/2022')
+# print(fin)
 
-# ticker = yf.Ticker('PETR4.SA')
-
-# df = ticker.history(period='30d')
-# df = df['Close']
-
-# df_ticker_daily = ticker.history(period='1d', interval='5m')
-# df_ticker_daily = df_ticker_daily['Close']
-
-# df = pd.DataFrame(df)
-# df_ticker_daily = pd.DataFrame(df_ticker_daily)
-
-
-
-# cotacao_bovespa['Adj Close'].plot()
-# plt.show()
-
-
-# dataframe1 = pd.read_csv("exemplo.txt")
-# # storing this dataframe in a csv file
-# dataframe1.to_csv('exemplo.csv', index = None)
-
-
-
-# data_inicial = "01/01/2020"
-# data_final = "01/01/2021"
-
-# empresas_df = pd.read_excel("Empresas.xlsx")
-# empresas_df
-
-# for empresa in empresas_df['Empresas']: 
-#     print(f"{empresa}:")
-#     df = web.DataReader(f'{empresa}.SA', data_source='yahoo', start=data_inicial, end=data_final)
-#     df
-#     df["Adj Close"].plot(figsize=(15, 10))
-#     plt.show()
+# print(seg)
